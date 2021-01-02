4d0e1b76e0bad757277cbe6beeb56b541ed07379
maxbarsukov <maximbarsukov@bk.ru>
2021-01-02 13:43:48 +0300

    more error pages added

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,13 +1,43 @@
 class ErrorsController < ApplicationController
+  def bad_request
+    respond_to do |format|
+      format.html { render status: 400 }
+    end
+  end
+
+  def unauthorized
+    respond_to do |format|
+      format.html { render status: 401 }
+    end
+  end
+
+  def forbidden
+    respond_to do |format|
+      format.html { render status: 403 }
+    end
+  end
+
   def not_found
     respond_to do |format|
       format.html { render status: 404 }
     end
   end
 
+  def request_timeout
+    respond_to do |format|
+      format.html { render status: 408 }
+    end
+  end
+
   def unacceptable
     respond_to do |format|
       format.html { render status: 422 }
+    end
+  end
+
+  def too_many_requests
+    respond_to do |format|
+      format.html { render status: 429 }
     end
   end
 
@@ -26,6 +56,12 @@ class ErrorsController < ApplicationController
   def service_unavailable
     respond_to do |format|
       format.html { render status: 503 }
+    end
+  end
+
+  def service_gateway_timeout
+    respond_to do |format|
+      format.html { render status: 504 }
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,16 @@ Rails.application.routes.draw do
   root to: 'entries#index'
 
   if Rails.env.production?
+    get '/400', to: 'errors#bad_request'
+    get '/401', to: 'errors#unauthorized'
+    get '/403', to: 'errors#forbidden'
     get '/404', to: 'errors#not_found'
+    get '/408', to: 'errors#request_timeout'
     get '/422', to: 'errors#unacceptable'
+    get '/429', to: 'errors#too_many_requests'
     get '/500', to: 'errors#internal_error'
     get '/502', to: 'errors#bad_gateway'
     get '/503', to: 'errors#service_unavailable'
+    get '/504', to: 'errors#service_gateway_timeout'
   end
 end
