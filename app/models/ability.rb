109cb73371f301ddba28ff521d659a01276bfdcb
maxbarsukov <maximbarsukov@bk.ru>
2021-01-03 20:09:08 +0300

    ability changed

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,6 +2,10 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+    user ||= User.new
+    can :read, Entry, user_id: user.id
+    can :manage, Entry, user_id: user.id
+
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
