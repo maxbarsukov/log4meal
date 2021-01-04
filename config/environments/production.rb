c83de6b89859f493014b72ef790ab36b02ed7a0d
maxbarsukov <maximbarsukov@bk.ru>
2021-01-04 10:52:28 +0300

    env updated

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,13 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "log4meal_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = {:host => 'localhost'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "127.0.0.1",
+    :port    => 25,
+    :domain  => 'localhostt'
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
